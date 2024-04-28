d11af8fac2a843054218274be3d1ac5e649ed45c
wiktor <wiktor.ziebka@externals.hilti.com>
2024-04-28 20:19:51 +0200

    Update and finish assignment 1

--- src/Cpp_Game_Engine.cpp
+++ src/Cpp_Game_Engine.cpp
@@ -8,54 +8,54 @@
 class AShape
 {
 public:
-    sf::CircleShape circle{};
-    sf::RectangleShape rectangle{};
+    sf::Shape *shape = nullptr;
     sf::Text text{};
+    std::string name;
+    float color[3];
+
+    float scale = 1.0f;
+    bool visible = true;
     float speedX, speedY;
 
     void checkBounce(int windowWidth, int windowHeight)
     {
-        auto& circlePos = circle.getPosition();
-        auto& rectSize = rectangle.getSize();
-        auto& rectanglePos = rectangle.getPosition();
-        auto circleRadius = circle.getRadius();
-        if (circleRadius > 0)
+        const sf::FloatRect& globalBounds = shape->getGlobalBounds();
+        if (globalBounds.left <= 0 || globalBounds.left + globalBounds.width >= windowWidth)
         {
-            if (circlePos.x <= 0 || circlePos.x + circleRadius * 2 >= windowWidth)
-            {
-                speedX = -speedX;
-            }
-            if (circlePos.y <= 0 || circlePos.y + circleRadius * 2 >= windowHeight)
-            {
-                speedY = -speedY;
-            }
+            speedX = -speedX;
         }
-        if (rectSize.x > 0 && rectSize.y > 0)
+        if (globalBounds.top <= 0 || globalBounds.top + globalBounds.height >= windowHeight)
         {
-            if (rectanglePos.x <= 0 || rectanglePos.x + rectSize.x >= windowWidth)
-            {
-                speedX = -speedX;
-            }
-
-            if (rectanglePos.y <= 0 || rectanglePos.y + rectSize.y >= windowHeight)
-            {
-                speedY = -speedY;
-            }
+            speedY = -speedY;
         }
     }
 
     void move()
     {
-        circle.move(speedX, speedY);
-        rectangle.move(speedX, speedY);
-        text.move(speedX, speedY);
+        shape->move(speedX, speedY);
+        setTextPosition();
     }
 
     void draw(sf::RenderWindow &window) const
     {
-        window.draw(circle);
-        window.draw(rectangle);
+        if (!visible)
+            return;
+
+        shape->setScale(scale, scale);
+        shape->setFillColor(sf::Color((color[0] * 255.0f), (color[1] * 255.0f), (color[2] * 255.0f)));
+
+        window.draw(*shape);
         window.draw(text);
+    }
+
+
+    void setTextPosition()
+    {
+        auto textBound = text.getLocalBounds();
+        const auto &shapeBounds = shape->getGlobalBounds();
+        float x = shapeBounds.left + shapeBounds.width / 2 - textBound.width / 2;
+        float y = shapeBounds.top + shapeBounds.height / 2 - textBound.height / 2;
+        text.setPosition(x, y);
     }
 };
 
@@ -100,47 +100,45 @@ int main()
             fin >> textSize >> r >> g >> b;
             textColor = sf::Color(r, g, b);
         }
-        if (configType == "Circle")
-        {
-            AShape shape{};
-            std::string name;
-            float x, y, speedX, speedY, radius;
-            int r, g, b;
-            fin >> name >> x >> y >> speedX >> speedY >> r >> g >> b >> radius;
-            shape.circle = sf::CircleShape(radius);
-            shape.circle.setFillColor(sf::Color(r, g, b));
-            shape.circle.setPosition(x, y);
-            shape.speedX = speedX;
-            shape.speedY = speedY;
-            shape.text = sf::Text(name, font, textSize);
-            auto bound = shape.text.getLocalBounds();
-            shape.text.setPosition(x + radius - bound.width / 2, y + radius - bound.height / 2);
-            shapes.push_back(shape);
-        }
-        if (configType == "Rectangle")
+        if (configType == "Circle" || configType == "Rectangle")
         {
             AShape shape{};
             std::string name;
             float x, y, speedX, speedY, width, height;
             int r, g, b;
-            fin >> name >> x >> y >> speedX >> speedY >> r >> g >> b >> width >> height;
-            shape.rectangle = sf::RectangleShape(sf::Vector2f(width, height));
-            shape.rectangle.setFillColor(sf::Color(r, g, b));
-            shape.rectangle.setPosition(x, y);
+            fin >> name >> x >> y >> speedX >> speedY >> r >> g >> b >> width;
+
+            if (configType == "Circle") {
+                shape.shape = new sf::CircleShape(width);
+            }
+            else 
+            {
+                fin >> height;
+                shape.shape = new sf::RectangleShape(sf::Vector2f(width, height));
+            }
+
+            sf::Color color = sf::Color(r, g, b);
+            shape.shape->setFillColor(color);
+            shape.shape->setPosition(x, y);
             shape.speedX = speedX;
             shape.speedY = speedY;
             shape.text = sf::Text(name, font, textSize);
-            auto bound = shape.text.getLocalBounds();
-            shape.text.setPosition(x + width / 2 - bound.width / 2, y + height / 2 - bound.height / 2);
+            shape.name = name;
+            shape.color[0] = r / 255.0f;
+            shape.color[1] = g / 255.0f;
+            shape.color[2] = b / 255.0f;
+            shape.setTextPosition();
             shapes.push_back(shape);
         }
     }
 
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML works!");
     ImGui::SFML::Init(window);
-    //window.setFramerateLimit(60);
+    window.setFramerateLimit(60);
 
     sf::Clock deltaClock;
+    int selectedShape = 0;
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -152,12 +150,41 @@ int main()
         }
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        // Dorobić ImGui, w którym będzie combobox do wybrania odpowiedniego kształtu, a także pola do:
-        // Zaznaczenia czy jest widoczny, zmiany koloru, Skalowania, Obu prędkości, Koloru i nazwy
-        ImGui::Begin("Shapes");
+        ImGui::Begin("UI");
+        std::vector<const char *> shapeNames;
+        for (const auto &shape : shapes)
+        {
+            shapeNames.push_back(shape.name.c_str());
+        }
+
+        if (ImGui::BeginCombo("Shapes", shapeNames[selectedShape]))
+        {
+            for (int n = 0; n < shapeNames.size(); n++)
+            {
+                bool isSelected = (selectedShape == n);
+                if (ImGui::Selectable(shapeNames[n], isSelected))
+                {
+                    selectedShape = n;
+                }
+                if (isSelected)
+                {
+                    ImGui::SetItemDefaultFocus();
+                }
+            }
+            ImGui::EndCombo();
+        }
+
+        if (shapes.size() > 0)
+        {
+            auto &selectedShapeObj = shapes[selectedShape];
+            ImGui::Checkbox("Visible", &selectedShapeObj.visible);
+            ImGui::ColorEdit3("Color", selectedShapeObj.color);
+            ImGui::SliderFloat("Scale", &selectedShapeObj.scale, 0.0f, 2.0f);
+            ImGui::InputFloat("SpeedX", &selectedShapeObj.speedX);
+            ImGui::InputFloat("SpeedY", &selectedShapeObj.speedY);
+        }
         ImGui::Text("Shapes");
         ImGui::End();
-
 
         window.clear();
         for (auto &shape : shapes)
