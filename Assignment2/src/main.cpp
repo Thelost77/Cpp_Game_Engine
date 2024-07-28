b71aa89bcf9d88fff64cb3e782f5ca3585f513d8
wiktor <wiktor.ziebka@externals.hilti.com>
2024-07-28 12:30:28 +0200

    master

--- Assignment2/src/main.cpp
+++ Assignment2/src/main.cpp
@@ -182,12 +182,13 @@
 
 // Should be updated to the correct paths when first used
 // Windows
-// #define FONT_PATH "C:\\Projekty\\Cpp_Game_Engine\\Cpp_Game_Engine\\src\\resources\\fonts\\"
-// #define CONFIG_PATH "C:\\Projekty\\Cpp_Game_Engine\\Cpp_Game_Engine\\src\\resources\\config.txt"
+
+#define FONT_PATH "C:\\Projekty\\Cpp_Game_Engine\\Assignment2\\src\\resources\\fonts\\"
+#define CONFIG_PATH "C:\\Projekty\\Cpp_Game_Engine\\Assignment2\\src\\resources\\config.txt"
 
 // Mac
-#define FONT_PATH "/Users/wiktorziebka/Projects/Cpp_Game_Engine/Assignment2/src/resources/fonts/"
-#define CONFIG_PATH "/Users/wiktorziebka/Projects/Cpp_Game_Engine/Assignment2/src/resources/config.txt"
+//#define FONT_PATH "/Users/wiktorziebka/Projects/Cpp_Game_Engine/Assignment2/src/resources/fonts/"
+//#define CONFIG_PATH "/Users/wiktorziebka/Projects/Cpp_Game_Engine/Assignment2/src/resources/config.txt"
 
 
 int main()
